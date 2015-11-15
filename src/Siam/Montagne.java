63cb008f84d450a49505527f1a9090759774e7da
Renaud Belloncle <renaud.belloncle@orange.fr>
2015-11-15 10:44:47 +0100

    Base - Faire rentrer pièce

--- src/Siam/Montagne.java
+++ src/Siam/Montagne.java
@@ -1,4 +1,8 @@
 package Siam;
 
 public class Montagne extends Piece {
+
+    public Montagne(Case position) {
+        super(position, 0);
+    }
 }
--- test/Siam/PieceUnitTest.java
+++ test/Siam/PieceUnitTest.java
@@ -1,4 +1,39 @@
 package Siam;
 
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 public class PieceUnitTest {
+
+    private Piece piece;
+
+    @Before
+    public void setUp() {
+        piece = new Piece(new Case(0,0), 0) {};
+    }
+
+    @Test
+    public void testGetPosition() {
+        Assert.assertEquals(0, piece.getPosition()[0]);
+        Assert.assertEquals(0, piece.getPosition()[1]);
+    }
+
+    @Test
+    public void testSetPosition() {
+        piece.setPosition(5,5);
+        Assert.assertEquals(5, piece.getPosition()[0]);
+        Assert.assertEquals(5, piece.getPosition()[1]);
+    }
+
+    @Test
+    public void testGetOrientation() {
+        Assert.assertEquals(0, piece.getOrientation());
+    }
+
+    @Test
+    public void testSetOrientation() {
+        piece.setOrientation(3);
+        Assert.assertEquals(3, piece.getOrientation());
+    }
 }
