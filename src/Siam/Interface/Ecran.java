ac18b833ad698891d8188050d9f7328664fc1047
Renaud Belloncle <renaud.belloncle@orange.fr>
2015-11-24 16:03:06 +0100

    Correctif code

--- src/Siam/Interface/Ecran.java
+++ src/Siam/Interface/Ecran.java
@@ -3,6 +3,7 @@ package Siam.Interface;
 import Siam.Constantes;
 
 public class Ecran implements Constantes {
+
     private int largeur, hauteur;
     private int[] pixels;
 
@@ -13,42 +14,12 @@ public class Ecran implements Constantes {
     }
 
     public void clear(){
-        for(int i =0; i < pixels.length; i++){
-            pixels[i] = 0;
-        }
+        for(int i =0; i < pixels.length; i++) pixels[i] = 0;
     }
 
     public int getPixel(int index){
         return pixels[index];
     }
-
-    public int getLargeur() {
-        return largeur;
-    }
-
-    public int getHauteur() {
-        return hauteur;
-    }
-
-    /*
-    //M�thode polyvalente
-    public void renderSprite(int xp, int yp, Sprite sprite, boolean xflip, boolean yflip){
-        for(int y = 0; y < sprite.getHauteur(); y++){
-            int ya = yp + y;
-            int ys = y;
-            if(yflip)ys = sprite.getHauteur()-y;
-            for(int x = 0; x < sprite.getLargeur(); x++){
-                int xs = x;
-                if(xflip) xs = sprite.getLargeur()-x;
-                int xa = xp + x;
-                if(xa < -sprite.getLargeur() || xa >= largeur || ya < 0 || ya >= hauteur) break;
-                if(xa < 0) xa = 0;
-                int col = sprite.getPixel(xs + ys * sprite.getLargeur());
-                if (col != 0xffff00ff) pixels[xa + ya * largeur] = col;
-            }
-        }
-    }
-     */
 
     // Prend les coordonnees d'un sprite et place ses pixels dans le tableau de pixel
     public void renderSprite(int xp, int yp, Sprite sprite, boolean xflip, boolean yflip) {
