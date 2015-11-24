91bfced73054bed7c11cee93ba283f20601170b7
MeganeBige <amane-yukio-sama@hotmail.fr>
2015-11-24 17:49:58 +0100

    Menu et Instructions

--- src/Siam/Appli.java
+++ src/Siam/Appli.java
@@ -1,11 +1,12 @@
 package Siam;
 
-import Siam.Interface.ChoixCamp;
+import Siam.Interface.Menu;
+
 
 public class Appli {
     public static void main(String[] args) {
         Game game = new Game();
-        new ChoixCamp(game, game.getFenetre());
+        Menu menu = new Menu(game, game.getFenetre());
 
     }
 }
